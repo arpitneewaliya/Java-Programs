54cacac1d1b5326d77ea2aa65f41de474f2b582e
Arpit Neewaliya <arpit.neewaliya@gmail.com>
2026-01-01 13:32:46 +0530

    First Java Program: Printing messages to the console.

--- Hello.java
+++ Hello.java
@@ -2,5 +2,15 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Fedora!");
         System.out.println("Hello Linux!");
+
+        System.out.print("Hello Fedora!");
+        System.out.print("Hello Linux!\n");
+
+        System.out.println(4);
+        System.out.println(4+5);
+        System.out.println(3 - 5);
+        System.out.println(3 * 4);
+        System.out.println(4 / 2);
+        
     }
 }
