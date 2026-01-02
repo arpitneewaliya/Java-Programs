e954837f7138f703a8bfecb325bc800e5f4774fa
Arpit Neewaliya <arpit.neewaliya@gmail.com>
2026-01-02 10:51:57 +0530

    Understanding classes and methods.

---
+++ Me.java
@@ -0,0 +1,10 @@
+public class Me {
+    
+    public static void sayMyName() {
+        System.out.println("John Wick");
+    }
+
+    public static void sayMyAge() {
+        System.out.println("20");
+    }
+}
